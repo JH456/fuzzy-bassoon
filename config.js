b4e08bd516c395c8a30bd0eaf95e0a7c3c1ddbc0
Jim <jharris9456@gmail.com>
2018-01-27 23:53:26 -0500

    debug

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 'use strict';
 
 let config = {
-    slackAPIToken: process.env.SLACK_API_TOKEN
-    mappingFile = process.env.MAPPING_FILE
+    slackAPIToken: process.env.SLACK_API_TOKEN,
+    mappingFile = process.env.MAPPING_FILE,
     callbackURL = process.env.CALLBACK_URL
 }
 
